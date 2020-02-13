5446cb93a599e4b8e9701311bd544cc4ecd55dcd
Ian Robinson <ianrob@amazon.com>
2020-02-13 10:23:35 +0000

    Add build file

---
+++ neptune-python-utils/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -ex
+
+pushd .
+pip install virtualenv
+rm -rf target
+rm -rf temp
+mkdir target
+virtualenv temp --python=python3.8
+source temp/bin/activate
+cd temp
+pip install gremlinpython
+pip install requests
+cd lib/python3.8/site-packages
+rm -rf certifi-*
+rm -rf easy_install.py
+rm -rf six.py
+cp -r ../../../../*.py .
+cp -r ../../../../neptune_python_utils .
+zip -r neptune_python_utils.zip neptune_python_utils gremlin_python aenum isodate tornado
+mv neptune_python_utils.zip ../../../../target/neptune_python_utils.zip
+deactivate
+popd
+rm -rf temp
