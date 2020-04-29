4953dd94f6afe41a7e5d669a39f6723574542832
Brad Bebee <beebs@amazon.com>
2020-04-29 07:40:31 -0700

    Revert "Support batch inserts/upserts in Glue jobs, with retries. (#54)"
    This reverts commit e7affa6e6f21f545afefcb596fa9901e055a8840.

--- neptune-python-utils/build.sh
+++ neptune-python-utils/build.sh
@@ -10,14 +10,13 @@ source temp/bin/activate
 cd temp
 pip install gremlinpython
 pip install requests
-pip install backoff
 cd lib/python3.8/site-packages
 rm -rf certifi-*
 rm -rf easy_install.py
 rm -rf six.py
 cp -r ../../../../*.py .
 cp -r ../../../../neptune_python_utils .
-zip -r neptune_python_utils.zip neptune_python_utils gremlin_python aenum isodate tornado backoff
+zip -r neptune_python_utils.zip neptune_python_utils gremlin_python aenum isodate tornado
 mv neptune_python_utils.zip ../../../../target/neptune_python_utils.zip
 deactivate
 popd
