e4ccc9c89bb597f9ff969cf5ae0be4a2c9aca5fd
Ian Robinson <ianrob@amazon.com>
2020-07-17 18:40:40 +0100

    Refactor package to software.amazon.neptune (#63)

--- neptune-gremlin-client/gremlin-client/src/main/java/software/amazon/neptune/cluster/EndpointsSelector.java
+++ neptune-gremlin-client/gremlin-client/src/main/java/software/amazon/neptune/cluster/EndpointsSelector.java
@@ -10,7 +10,7 @@ express or implied. See the License for the specific language governing
 permissions and limitations under the License.
 */
 
-package com.amazonaws.services.neptune.cluster;
+package software.amazon.neptune.cluster;
 
 import java.util.Collection;
 import java.util.Map;
