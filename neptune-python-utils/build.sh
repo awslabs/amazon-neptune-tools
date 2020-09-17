3896555b760ebdd9fc5787fbcaa06b775f4aed54
Ian Robinson <ianrob@amazon.com>
2020-09-17 21:05:47 +0100

    Add neptune-python-utils to release

--- neptune-python-utils/build.sh
+++ neptune-python-utils/build.sh
@@ -15,7 +15,6 @@ cd lib/python3.8/site-packages
 rm -rf certifi-*
 rm -rf easy_install.py
 rm -rf six.py
-cp -r ../../../../*.py .
 cp -r ../../../../neptune_python_utils .
 zip -r neptune_python_utils.zip neptune_python_utils gremlin_python aenum isodate tornado backoff
 mv neptune_python_utils.zip ../../../../target/neptune_python_utils.zip
