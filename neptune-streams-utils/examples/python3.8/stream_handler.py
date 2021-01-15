22b645356fb53ea5632b1e6807a0de4be5819ff6
Ian Robinson <ianrob@amazon.com>
2021-01-15 10:11:20 +0000

    Update license

--- neptune-streams-utils/examples/python3.8/stream_handler.py
+++ neptune-streams-utils/examples/python3.8/stream_handler.py
@@ -1,7 +1,3 @@
-'''
-Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-SPDX-License-Identifier: MIT-0
-'''
 # Copyright Amazon.com, Inc. or its affiliates.
 # All Rights Reserved.
 #
