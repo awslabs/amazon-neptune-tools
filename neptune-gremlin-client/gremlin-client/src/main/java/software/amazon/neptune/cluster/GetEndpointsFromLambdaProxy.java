b03fcc10fba1b9d546fe0578c5e6cc8b12611202
Ian Robinson <ianrob@amazon.com>
2022-11-18 13:59:47 +0000

    Meaning error message when Lambda proxy doesn't support returning JSON. (#259)
    Co-authored-by: ianrob <ianrob@amazon.co.uk>

--- neptune-gremlin-client/gremlin-client/src/main/java/software/amazon/neptune/cluster/GetEndpointsFromLambdaProxy.java
+++ neptune-gremlin-client/gremlin-client/src/main/java/software/amazon/neptune/cluster/GetEndpointsFromLambdaProxy.java
@@ -25,7 +25,8 @@ import com.evanlennick.retry4j.CallExecutorBuilder;
 import com.evanlennick.retry4j.Status;
 import com.evanlennick.retry4j.config.RetryConfig;
 import com.evanlennick.retry4j.config.RetryConfigBuilder;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.evanlennick.retry4j.exception.UnexpectedException;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.tinkerpop.gremlin.driver.IamAuthConfig;
 import software.amazon.utils.RegionUtils;
@@ -128,58 +129,82 @@ public class GetEndpointsFromLambdaProxy implements ClusterEndpointsFetchStrateg
     @Override
     public Map<EndpointsSelector, Collection<String>> getAddresses() {
 
-        if (endpointsSelector.getClass().equals(EndpointsType.class)) {
-            Callable<Map<EndpointsSelector, Collection<String>>> query = () -> {
-
-                EndpointsType endpointsType = (EndpointsType) endpointsSelector;
-
-                InvokeRequest invokeRequest = new InvokeRequest()
-                        .withFunctionName(lambdaName)
-                        .withPayload(String.format("\"%s\"", endpointsType.name()));
-
-                InvokeResult result = lambdaClient.invoke(invokeRequest);
-                String payload = new String(result.getPayload().array());
-
-                Map<EndpointsSelector, Collection<String>> results = new HashMap<>();
-
-                results.put(endpointsType, Arrays.asList(payload.split(",")));
-
-                return results;
-            };
-
-            @SuppressWarnings("unchecked")
-            CallExecutor<Map<EndpointsSelector, Collection<String>>> executor =
-                    new CallExecutorBuilder<Map<EndpointsSelector, Collection<String>>>().config(retryConfig).build();
-
-            Status<Map<EndpointsSelector, Collection<String>>> status = executor.execute(query);
-
-            return status.getResult();
+        if (EndpointsType.class.isAssignableFrom(endpointsSelector.getClass())) {
+            return getAddressesForEndpointsType();
         } else {
-            Callable<NeptuneClusterMetadata> query = () -> {
+            return getAddressesForCustomEndpointsSelector();
+        }
+    }
 
-                InvokeRequest invokeRequest = new InvokeRequest()
-                        .withFunctionName(lambdaName)
-                        .withPayload("\"\"");
-                InvokeResult result = lambdaClient.invoke(invokeRequest);
+    private Map<EndpointsSelector, Collection<String>> getAddressesForCustomEndpointsSelector() {
+        Callable<NeptuneClusterMetadata> query = () -> {
 
-                return NeptuneClusterMetadata.fromByeArray(result.getPayload().array());
-            };
+            InvokeRequest invokeRequest = new InvokeRequest()
+                    .withFunctionName(lambdaName)
+                    .withPayload("\"\"");
+            InvokeResult result = lambdaClient.invoke(invokeRequest);
 
-            @SuppressWarnings("unchecked")
-            CallExecutor<NeptuneClusterMetadata> executor =
-                    new CallExecutorBuilder<Map<EndpointsSelector, Collection<String>>>().config(retryConfig).build();
+            return NeptuneClusterMetadata.fromByeArray(result.getPayload().array());
+        };
 
-            Status<NeptuneClusterMetadata> status = executor.execute(query);
-            NeptuneClusterMetadata neptuneClusterMetadata = status.getResult();
+        @SuppressWarnings("unchecked")
+        CallExecutor<NeptuneClusterMetadata> executor =
+                new CallExecutorBuilder<Map<EndpointsSelector, Collection<String>>>().config(retryConfig).build();
+
+        Status<NeptuneClusterMetadata> status;
+
+        try {
+            status = executor.execute(query);
+        } catch (UnexpectedException e){
+            if (e.getCause() instanceof MismatchedInputException){
+                throw new IllegalStateException(String.format("The AWS Lambda proxy (%s) isn't returning a NeptuneClusterMetadata JSON document. Check that the function supports returning a NeptuneClusterMetadata JSON document.", lambdaName), e.getCause());
+            } else{
+                throw new IllegalStateException(String.format("There was an unexpected error while attempting to get a NeptuneClusterMetadata JSON document from the AWS Lambda proxy (%s). Check that the function supports returning a NeptuneClusterMetadata JSON document.", lambdaName), e.getCause());
+            }
+        }
+
+        NeptuneClusterMetadata neptuneClusterMetadata = status.getResult();
+        Map<EndpointsSelector, Collection<String>> results = new HashMap<>();
+
+        results.put(endpointsSelector, endpointsSelector.getEndpoints(
+                neptuneClusterMetadata.getClusterEndpoint(),
+                neptuneClusterMetadata.getReaderEndpoint(),
+                neptuneClusterMetadata.getInstances()));
+
+        return results;
+    }
+
+    private Map<EndpointsSelector, Collection<String>> getAddressesForEndpointsType() {
+        Callable<Map<EndpointsSelector, Collection<String>>> query = () -> {
+
+            EndpointsType endpointsType = (EndpointsType) endpointsSelector;
+
+            InvokeRequest invokeRequest = new InvokeRequest()
+                    .withFunctionName(lambdaName)
+                    .withPayload(String.format("\"%s\"", endpointsType.name()));
+
+            InvokeResult result = lambdaClient.invoke(invokeRequest);
+            String payload = new String(result.getPayload().array());
 
             Map<EndpointsSelector, Collection<String>> results = new HashMap<>();
 
-                results.put(endpointsSelector, endpointsSelector.getEndpoints(
-                        neptuneClusterMetadata.getClusterEndpoint(),
-                        neptuneClusterMetadata.getReaderEndpoint(),
-                        neptuneClusterMetadata.getInstances()));
+            results.put(endpointsType, Arrays.asList(payload.split(",")));
 
             return results;
+        };
+
+        @SuppressWarnings("unchecked")
+        CallExecutor<Map<EndpointsSelector, Collection<String>>> executor =
+                new CallExecutorBuilder<Map<EndpointsSelector, Collection<String>>>().config(retryConfig).build();
+
+        Status<Map<EndpointsSelector, Collection<String>>> status;
+
+        try{
+            status = executor.execute(query);
+        } catch (UnexpectedException e){
+            throw new IllegalStateException(String.format("There was an unexpected error while attempting to get a list of endpoints from the AWS Lambda proxy (%s). Check that the function supports returning a list of endpoints.", lambdaName), e.getCause());
         }
+
+        return status.getResult();
     }
 }
