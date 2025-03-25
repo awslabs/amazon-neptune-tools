e77253dfd5378f0816acb18d1e72d401fa15f7f9
Stephen Mallette <stepmall@amazon.com>
2025-03-25 07:41:28 -0400

    Remove code from deprecated libraries.
    Both neptune-export and neptune-gremlin-client were long ago deprecated from this repository. Retaining the READMEs as a pointer to the new locations for these packages.

--- neptune-gremlin-client/gremlin-client/src/main/java/software/amazon/neptune/cluster/OnNewClusterMetadata.java
+++
@@ -1,8 +0,0 @@
-package software.amazon.neptune.cluster;
-
-import java.util.Collection;
-import java.util.Map;
-
-public interface OnNewClusterMetadata {
-    void apply(NeptuneClusterMetadata neptuneClusterMetadata);
-}
