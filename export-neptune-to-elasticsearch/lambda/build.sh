4e107054335e41f025be97f219cc7bb1e980deed
Taylor Riggan <triggan@amazon.com>
2026-01-06 15:37:45 -0500

    Updating runtimes to newer/supported versions. Added KMS CMK support. Added RDF with IAM auth support.

--- export-neptune-to-elasticsearch/lambda/build.sh
+++ export-neptune-to-elasticsearch/lambda/build.sh
@@ -4,10 +4,10 @@ pip install virtualenv
 rm -rf target
 rm -rf temp
 mkdir target
-virtualenv temp --python=python3.8
+virtualenv temp --python=python3.11
 source temp/bin/activate
 pip install -r requirements.txt
-cd temp/lib/python3.8/site-packages
+cd temp/lib/python3.11/site-packages
 cp -r ../../../../*.py .
 zip -r ../../../../target/export-neptune-to-elasticsearch.zip ./*
 deactivate
